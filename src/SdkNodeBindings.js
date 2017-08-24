ca122c2bd39dc516785cc8501c386388c3a5e2ce
Allan Tatter <allan.tatter@gmail.com>
2017-08-24 22:34:05 +0300

    Fix path when loading local bindings

--- src/SdkNodeBindings.js
+++ src/SdkNodeBindings.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export default function getSdkNodeBindings() {
-  if (fs.existsSync(path.resolve(__dirname, 'sdk-node-bindings', 'lib', 'binding.js'))) {
+  if (fs.existsSync(path.resolve(__dirname, '..', 'sdk-node-bindings', 'lib', 'binding.js'))) {
     return require('../sdk-node-bindings/lib/binding.js');
 
   } else if (process.platform === 'darwin') {
