01550c0ad2e5c653140b5b0fa2a55207eb5e10a8
Allan Tatter <allan.tatter@gmail.com>
2017-08-29 09:57:35 +0300

    Remove unnecessary directories from repo

--- es/Permissions.js
+++
@@ -1,9 +0,0 @@
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Permissions = function Permissions(bytes) {
-  _classCallCheck(this, Permissions);
-
-  this.bytes = bytes;
-};
-
-export default Permissions;
