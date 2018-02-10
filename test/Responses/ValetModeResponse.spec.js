c88e12abe17057031679ea4aa0c4873ac922f49c
Matis Lepik <matis.lepik@gmail.com>
2018-02-10 14:59:27 +0200

    Fix valet mode test structure

--- test/Responses/ValetModeResponse.spec.js
+++ test/Responses/ValetModeResponse.spec.js
@@ -4,16 +4,16 @@ import { hexToUint8Array } from '../../src/encoding';
 
 describe(`ValetModeResponse`, () => {
   it(`should return ValetModeResponse`, () => {
-    const response = new Response(hexToUint8Array('00280101000101'));
+    let response = new Response(hexToUint8Array('00280101000101'));
     expect(response.parse()).toBeInstanceOf(ValetModeResponse);
     expect(response.parse()).toEqual({
       mode: 'activated',
     });
-  });
 
-  const response = new Response(hexToUint8Array('00280101000100'));
-  expect(response.parse()).toBeInstanceOf(ValetModeResponse);
-  expect(response.parse()).toEqual({
-    mode: 'deactivated',
+    response = new Response(hexToUint8Array('00280101000100'));
+    expect(response.parse()).toBeInstanceOf(ValetModeResponse);
+    expect(response.parse()).toEqual({
+      mode: 'deactivated',
+    });
   });
 });
