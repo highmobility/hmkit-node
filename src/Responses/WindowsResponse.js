77ef32803363022651c68b1970fcfda0f75c12f7
Matis Lepik <matis.lepik@gmail.com>
2018-02-10 14:03:51 +0200

    Update WindowsResponse

--- src/Responses/WindowsResponse.js
+++ src/Responses/WindowsResponse.js
@@ -9,11 +9,22 @@ export default class WindowsResponse extends PropertyResponse {
     super();
 
     const properties = [
-      new Property(0x00, 'frontLeft').setDecoder(this.openClosedDecoder()),
-      new Property(0x01, 'frontRight').setDecoder(this.openClosedDecoder()),
-      new Property(0x02, 'rearRight').setDecoder(this.openClosedDecoder()),
-      new Property(0x03, 'rearLeft').setDecoder(this.openClosedDecoder()),
-      new Property(0x04, 'hatch').setDecoder(this.openClosedDecoder())
+      new Property(0x01, 'windows')
+        .setSubProperty(
+          new Property(0x00, 'frontLeft').setDecoder(this.openClosedDecoder())
+        )
+        .setSubProperty(
+          new Property(0x01, 'frontRight').setDecoder(this.openClosedDecoder())
+        )
+        .setSubProperty(
+          new Property(0x02, 'rearRight').setDecoder(this.openClosedDecoder())
+        )
+        .setSubProperty(
+          new Property(0x03, 'rearLeft').setDecoder(this.openClosedDecoder())
+        )
+        .setSubProperty(
+          new Property(0x04, 'hatch').setDecoder(this.openClosedDecoder())
+        ),
     ];
 
     this.parse(data, properties);
@@ -22,7 +33,7 @@ export default class WindowsResponse extends PropertyResponse {
   openClosedDecoder() {
     return switchDecoder({
       0x00: 'closed',
-      0x01: 'open'
+      0x01: 'open',
     });
   }
 }
--- test/Responses/WindowsResponse.spec.js
+++ test/Responses/WindowsResponse.spec.js
@@ -3,17 +3,22 @@ import WindowsResponse from '../../src/Responses/WindowsResponse';
 import { hexToUint8Array } from '../../src/encoding';
 
 describe(`WindowsResponse`, () => {
-  it(`should return WindowsResponse`, () => {
-    const response = new Response(hexToUint8Array('004501040001010002010300'));
+  it.only(`should return WindowsResponse`, () => {
+    const response = new Response(
+      hexToUint8Array('0045010100020001010002010001000202010100020300')
+    );
 
     expect(response.parse()).toBeInstanceOf(WindowsResponse);
 
     expect(response.parse()).toEqual(
       expect.objectContaining({
-        frontLeft: 'open',
-        frontRight: 'closed',
-        rearRight: 'open',
-        rearLeft: 'closed',
+        windows: {
+          frontLeft: 'open',
+          frontRight: 'closed',
+          rearRight: 'open',
+          rearLeft: 'closed',
+          hatch: null,
+        },
       })
     );
   });
