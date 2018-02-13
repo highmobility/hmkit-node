099df5f24c1d1f5d0dc19cc11c33a79fa209de64
Mikk Rätsep <mikk.ratsep@gmail.com>
2018-02-13 21:30:55 +0200

    ParkingBrake is ParkingBrake, ParkingBrake is not Engine

--- src/Responses/ParkingBrakeResponse.js
+++ src/Responses/ParkingBrakeResponse.js
@@ -2,7 +2,7 @@ import PropertyResponse from '../PropertyResponse';
 import Property from '../Property';
 import { activeInactiveDecoder } from '../helpers';
 
-export default class EngineResponse extends PropertyResponse {
+export default class ParkingBrakeResponse extends PropertyResponse {
   static identifier = [0x00, 0x58];
 
   constructor(data: Uint8Array) {
