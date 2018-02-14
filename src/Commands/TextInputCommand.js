09fe22fc06e06310e5a026e5d8b8a2f3ed9911de
Mikk Rätsep <mikk.ratsep@gmail.com>
2018-02-14 17:16:53 +0200

    TextInput fix

--- src/Commands/TextInputCommand.js
+++ src/Commands/TextInputCommand.js
@@ -1,10 +1,10 @@
 import Command from './Command';
-import { sizeBytes, stringToBytes } from '../encoding';
+import { intToTwoBytes, stringToBytes } from '../encoding';
 
 export default class TextInputCommand {
   static textInput(text) {
     const textBytes = stringToBytes(text);
-      const lengthBytes = sizeBytes(textBytes);
+      const lengthBytes = intToTwoBytes(textBytes.length);
 
     return new Command([0x00, 0x44, 0x00, 0x01, ...lengthBytes, ...textBytes]);
   }
