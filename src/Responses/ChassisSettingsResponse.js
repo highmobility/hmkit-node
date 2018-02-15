62b357fa582ee13c49554d2ab824f4ed44cef57d
Mikk <mikkoun@gmail.com>
2018-02-15 15:54:47 +0200

    Added optional properties support.

--- src/Responses/ChassisSettingsResponse.js
+++ src/Responses/ChassisSettingsResponse.js
@@ -1,5 +1,6 @@
 import PropertyResponse from '../PropertyResponse';
 import Property from '../Property';
+import OptionalProperty from '../OptionalProperty';
 import { uint8toInt8 } from '../encoding';
 import { switchDecoder } from '../helpers';
 
@@ -15,28 +16,23 @@ export default class ChassisSettingsResponse extends PropertyResponse {
           0x00: 'regular',
           0x01: 'eco',
           0x02: 'sport',
-          0x03: 'sport_plus',
+          0x03: 'sport_plus'
         })
       ),
 
       new Property(0x02, 'sportChrono').setDecoder(
         switchDecoder({
           0x00: 'inactive',
-          0x01: 'active',
+          0x01: 'active'
         })
       ),
 
-      new Property(0x03, 'springRates')
-        .setSubProperty(
-          new Property(0x00, 'front').setDecoder(this.springRateDecoder)
-        )
-        .setSubProperty(
-          new Property(0x01, 'rear').setDecoder(this.springRateDecoder)
-        ),
+      new Property(0x03, 'springRates').setOptionalSubProperties('axle', [
+        new OptionalProperty(0x00, 'front_axle').setDecoder(this.springRateDecoder),
+        new OptionalProperty(0x01, 'rear_axle').setDecoder(this.springRateDecoder)
+      ]),
 
-      new Property(0x04, 'chassisPosition').setDecoder(
-        this.chassisPositionDecoder
-      ),
+      new Property(0x04, 'chassisPosition').setDecoder(this.chassisPositionDecoder)
     ];
 
     this.parse(data, properties);
@@ -44,17 +40,17 @@ export default class ChassisSettingsResponse extends PropertyResponse {
 
   chassisPositionDecoder(bytes: Array<Number>) {
     return {
-      position: uint8toInt8(bytes[0]),
-      maximum: uint8toInt8(bytes[1]),
-      minimum: uint8toInt8(bytes[2]),
+      chassisPosition: uint8toInt8(bytes[0]),
+      maximumValue: uint8toInt8(bytes[1]),
+      minimumValue: uint8toInt8(bytes[2])
     };
   }
 
   springRateDecoder(bytes: Array<Number>) {
     return {
-      rate: uint8toInt8(bytes[0]),
-      maximum: uint8toInt8(bytes[1]),
-      minimum: uint8toInt8(bytes[2]),
+      springRate: uint8toInt8(bytes[0]),
+      maximumValue: uint8toInt8(bytes[1]),
+      minimumValue: uint8toInt8(bytes[2])
     };
   }
 }
