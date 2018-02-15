77699de7695bafaa29a2bb4b8e622f2076828acf
Mikk Rätsep <mikk.ratsep@gmail.com>
2018-02-15 14:38:25 +0200

    Graphics lvl 6; Browser code formatting

--- src/Commands/BrowserCommand.js
+++ src/Commands/BrowserCommand.js
@@ -3,11 +3,15 @@ import { intToTwoBytes, stringToBytes } from '../encoding';
 
 export default class BrowserCommand {
   static loadUrl(url) {
-      const urlBytes = stringToBytes(url);
+    const urlBytes = stringToBytes(url);
 
-      return new Command([0x00, 0x49,
-                          0x00,
-                          0x01, ...intToTwoBytes(urlBytes.length), ...urlBytes
-                          ])
+    return new Command([
+      0x00,
+      0x49,
+      0x00,
+      0x01,
+      ...intToTwoBytes(urlBytes.length),
+      ...urlBytes,
+    ]);
   }
 }
