c62d3a7adba24beebd3a566aedaf37b1d32c1068
Mikk Rätsep <mikk.ratsep@gmail.com>
2018-02-19 12:45:56 +0200

    Race command tests

--- test/Commands/RaceCommand.spec.js
+++ test/Commands/RaceCommand.spec.js
@@ -10,5 +10,34 @@ describe(`RaceCommand`, () => {
     );
 
     expect(response.parse()).toBeInstanceOf(RaceResponse);
+    expect(response.parse()).toEqual({
+      accelerations: expect.objectContaining([
+        {
+          type: 'longitudinal_acceleration',
+          gForce: expect.any(Number),
+        },
+      ]),
+      brakePedalPosition: expect.any(Number),
+      brakePressure: expect.any(Number),
+      brakeTorqueVectorings: expect.objectContaining([
+        {
+          axle: 'front_axle',
+          vectoring: expect.any(String),
+        },
+        {
+          axle: 'rear_axle',
+          vectoring: expect.any(String),
+        },
+      ]),
+      gasPedalPosition: expect.any(Number),
+      gearMode: expect.any(String),
+      electronicStabilityProgram: expect.any(String),
+      oversteering: expect.any(Number),
+      rearSuspensionSteering: expect.any(Number),
+      selectedGear: expect.any(Number),
+      steeringAngle: expect.any(Number),
+      understeering: expect.any(Number),
+      yawRate: expect.any(Number),
+    });
   });
 });
