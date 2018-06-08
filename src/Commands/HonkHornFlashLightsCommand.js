710e25e64cd87df5ba06833de3809549d2d1b29b
Mikk <Mikkoun@gmail.com>
2018-06-08 15:18:06 +0300

    NO-NODE - Fixed honk and flash command building.

--- src/Commands/HonkHornFlashLightsCommand.js
+++ src/Commands/HonkHornFlashLightsCommand.js
@@ -15,19 +15,12 @@ export default class HonkHornFlashLightsCommand {
    * @property {Number} flashLights (number) Number of times
    */
   static honkHornFlashLights(honkHorn: number, flashLights: number) {
-    return new Command([
-      0x00,
-      0x26,
-      0x02,
-      0x01,
-      0x00,
-      0x01,
-      honkHorn,
-      0x02,
-      0x00,
-      0x01,
-      flashLights,
-    ]);
+    const commandBase = [0x00, 0x26, 0x02];
+
+    if (!!honkHorn) commandBase.push(0x01, 0x00, 0x01, honkHorn);
+    if (!!flashLights) commandBase.push(0x02, 0x00, 0x01, flashLights);
+
+    return new Command(commandBase);
   }
 
   /**
