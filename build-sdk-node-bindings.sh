a781aaf7476046d6c54ef511017edb26f3229e76
Shanmugasarathy <shanmugasarathy@high-mobility.com>
2018-06-12 08:34:05 +0200

    updated macos.node, sdk binding branch for multi instance

--- build-sdk-node-bindings.sh
+++ build-sdk-node-bindings.sh
@@ -1,7 +1,7 @@
 rm -rf sdk-node-bindings
 git clone git@bitbucket.org:highmobility/sdk-node-bindings.git
 cd sdk-node-bindings
-git checkout BC-105
+git checkout multi_inst
 git submodule update --init --recursive
 npm install
 
