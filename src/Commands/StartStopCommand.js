78c02ba1e0535859abd1dd8308b7a53516c82945
Mikk <Mikkoun@gmail.com>
2018-10-30 14:50:42 +0200

    NODE-121 - StartStop improvements

--- src/Commands/StartStopCommand.js
+++ src/Commands/StartStopCommand.js
@@ -1,6 +1,7 @@
 import Command from './Command';
+import BaseCommand from './BaseCommand';
 
-export default class StartStopCommand {
+export default class StartStopCommand extends BaseCommand {
   /**
    * @function getState
    */
@@ -12,13 +13,13 @@ export default class StartStopCommand {
    * @function activate
    */
   static activate() {
-    return new Command([0x00, 0x63, 0x02, 0x01, 0x00, 0x01, 0x01]);
+    return new Command([0x00, 0x63, 0x12, ...this.buildProperty(0x01, 0x01)]);
   }
 
   /**
    * @function deactivate
    */
   static deactivate() {
-    return new Command([0x00, 0x63, 0x02, 0x01, 0x00, 0x01, 0x00]);
+    return new Command([0x00, 0x63, 0x12, ...this.buildProperty(0x01, 0x00)]);
   }
 }
