5594d638a7e75830c015f4d09c5b835180480dc4
Mikk <Mikkoun@gmail.com>
2018-11-19 11:32:13 +0200

    NO-NODE - Added missing vehicle status properties conf.

--- src/Responses/VehicleStatusResponse.js
+++ src/Responses/VehicleStatusResponse.js
@@ -25,6 +25,9 @@ export default class VehicleStatusResponse extends PropertyResponse {
    * @property {Number} engineVolume (number) The engine volume displacement in liters
    * @property {Number} engineMaxTorque (number) The maximum engine torque in Nm
    * @property {String} gearbox (string 'manual|automatic|semi_automatic') Gearbox
+   * @property {String} displayUnit (string 'km|miles') Display unit
+   * @property {String} driverSeatLocation (string 'left|right|center') Driver seat location
+   * @property {String} equipments (array) List of equipments
    * @property {String} states (array '{capabilityIdentifier: (string), state: (object)}') Capability states
    *
    * @example VehicleStatusResponse
@@ -43,6 +46,9 @@ export default class VehicleStatusResponse extends PropertyResponse {
       engineVolume: 4395,
       engineMaxTorque: 520,
       gearbox: 'semi_automatic',
+      displayUnit: 'km',
+      driverSeatLocation: 'left',
+      equipments: ['side skirts', 'front apron', 'rear apron'],
       states: [
         {
           capabilityIdentifier: 'trunk',
