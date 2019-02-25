c3ea2fe33baf8a4c4fc2c004c6f80cedcc3407af
Mikk <Mikkoun@gmail.com>
2019-02-25 14:17:56 +0200

    KIT-35 - Fixed diagnostics tests.

--- test/Commands/DiagnosticsCommand.spec.js
+++ test/Commands/DiagnosticsCommand.spec.js
@@ -13,63 +13,205 @@ describe(`DiagnosticsCommand`, () => {
 
     expect(response.parse()).toEqual(
       expect.objectContaining({
-        mileage: expect.any(Number),
-        engineOilTemperature: expect.any(Number),
-        speed: expect.any(Number),
-        engineRPM: expect.any(Number),
-        fuelLevel: expect.any(Number),
-        estimatedRange: expect.any(Number),
-        washerFluidLevel: expect.any(String),
-        batteryVoltage: expect.any(Number),
-        adblueLevel: expect.any(Number),
-        distanceSinceReset: expect.any(Number),
-        distanceSinceStart: expect.any(Number),
-        fuelVolume: expect.any(Number),
-        antiLockBraking: expect.any(String),
-        engineCoolantTemperature: expect.any(Number),
-        engineTotalOperatingHours: expect.any(Number),
-        engineTotalFuelConsumption: expect.any(Number),
-        brakeFluidLevel: expect.any(String),
-        engineTorque: expect.any(Number),
-        engineLoad: expect.any(Number),
-        wheelBasedSpeed: expect.any(Number),
-        batteryLevel: expect.any(Number),
+        mileage: { data: expect.any(Number), timestamp: expect.any(Date) },
+        engineOilTemperature: {
+          data: expect.any(Number),
+          timestamp: expect.any(Date),
+        },
+        speed: { data: expect.any(Number), timestamp: expect.any(Date) },
+        engineRPM: { data: expect.any(Number), timestamp: expect.any(Date) },
+        fuelLevel: { data: expect.any(Number), timestamp: expect.any(Date) },
+        estimatedRange: {
+          data: expect.any(Number),
+          timestamp: expect.any(Date),
+        },
+        washerFluidLevel: {
+          data: expect.any(String),
+          timestamp: expect.any(Date),
+        },
+        batteryVoltage: {
+          data: expect.any(Number),
+          timestamp: expect.any(Date),
+        },
+        adblueLevel: { data: expect.any(Number), timestamp: expect.any(Date) },
+        distanceSinceReset: {
+          data: expect.any(Number),
+          timestamp: expect.any(Date),
+        },
+        distanceSinceStart: {
+          data: expect.any(Number),
+          timestamp: expect.any(Date),
+        },
+        fuelVolume: { data: expect.any(Number), timestamp: expect.any(Date) },
+        antiLockBraking: {
+          data: expect.any(String),
+          timestamp: expect.any(Date),
+        },
+        engineCoolantTemperature: {
+          data: expect.any(Number),
+          timestamp: expect.any(Date),
+        },
+        engineTotalOperatingHours: {
+          data: expect.any(Number),
+          timestamp: expect.any(Date),
+        },
+        engineTotalFuelConsumption: {
+          data: expect.any(Number),
+          timestamp: expect.any(Date),
+        },
+        brakeFluidLevel: {
+          data: expect.any(String),
+          timestamp: expect.any(Date),
+        },
+        engineTorque: { data: expect.any(Number), timestamp: expect.any(Date) },
+        engineLoad: { data: expect.any(Number), timestamp: expect.any(Date) },
+        wheelBasedSpeed: {
+          data: expect.any(Number),
+          timestamp: expect.any(Date),
+        },
+        batteryLevel: { data: expect.any(Number), timestamp: expect.any(Date) },
         checkControlMessages: expect.objectContaining([
           {
-            id: expect.any(Number),
-            remainingMinutes: expect.any(Number),
-            status: expect.any(String),
-            text: expect.any(String),
+            data: {
+              id: expect.any(Number),
+              remainingMinutes: expect.any(Number),
+              text: expect.any(String),
+              status: expect.any(String),
+            },
+            timestamp: expect.any(Date),
           },
         ]),
         tirePressures: [
-          { location: 'front_left', pressure: expect.any(Number) },
-          { location: 'front_right', pressure: expect.any(Number) },
-          { location: 'rear_right', pressure: expect.any(Number) },
-          { location: 'rear_left', pressure: expect.any(Number) },
+          {
+            data: { location: 'front_left', pressure: expect.any(Number) },
+            timestamp: expect.any(Date),
+          },
+          {
+            data: { location: 'front_right', pressure: expect.any(Number) },
+            timestamp: expect.any(Date),
+          },
+          {
+            data: { location: 'rear_right', pressure: expect.any(Number) },
+            timestamp: expect.any(Date),
+          },
+          {
+            data: { location: 'rear_left', pressure: expect.any(Number) },
+            timestamp: expect.any(Date),
+          },
         ],
         tireTemperatures: [
-          { location: 'front_left', temperature: expect.any(Number) },
-          { location: 'front_right', temperature: expect.any(Number) },
-          { location: 'rear_right', temperature: expect.any(Number) },
-          { location: 'rear_left', temperature: expect.any(Number) },
+          {
+            data: { location: 'front_left', temperature: expect.any(Number) },
+            timestamp: expect.any(Date),
+          },
+          {
+            data: { location: 'front_right', temperature: expect.any(Number) },
+            timestamp: expect.any(Date),
+          },
+          {
+            data: { location: 'rear_right', temperature: expect.any(Number) },
+            timestamp: expect.any(Date),
+          },
+          {
+            data: { location: 'rear_left', temperature: expect.any(Number) },
+            timestamp: expect.any(Date),
+          },
         ],
         wheelRpms: [
-          { location: 'front_left', rpm: expect.any(Number) },
-          { location: 'front_right', rpm: expect.any(Number) },
-          { location: 'rear_right', rpm: expect.any(Number) },
-          { location: 'rear_left', rpm: expect.any(Number) },
+          {
+            data: { location: 'front_left', rpm: expect.any(Number) },
+            timestamp: expect.any(Date),
+          },
+          {
+            data: { location: 'front_right', rpm: expect.any(Number) },
+            timestamp: expect.any(Date),
+          },
+          {
+            data: { location: 'rear_right', rpm: expect.any(Number) },
+            timestamp: expect.any(Date),
+          },
+          {
+            data: { location: 'rear_left', rpm: expect.any(Number) },
+            timestamp: expect.any(Date),
+          },
         ],
         troubleCodes: expect.objectContaining([
           {
-            occurences: expect.any(Number),
-            id: expect.any(String),
-            ecuId: expect.any(String),
-            status: expect.any(String),
+            data: {
+              occurences: expect.any(Number),
+              id: expect.any(String),
+              ecuId: expect.any(String),
+              status: expect.any(String),
+            },
+            timestamp: expect.any(Date),
           },
         ]),
-        mileageMeters: expect.any(Number),
+        mileageMeters: {
+          data: expect.any(Number),
+          timestamp: expect.any(Date),
+        },
       })
     );
+
+    // expect(response.parse()).toEqual(
+    //   expect.objectContaining({
+    //     mileage: expect.any(Number),
+    //     engineOilTemperature: expect.any(Number),
+    //     speed: expect.any(Number),
+    //     engineRPM: expect.any(Number),
+    //     fuelLevel: expect.any(Number),
+    //     estimatedRange: expect.any(Number),
+    //     washerFluidLevel: expect.any(String),
+    //     batteryVoltage: expect.any(Number),
+    //     adblueLevel: expect.any(Number),
+    //     distanceSinceReset: expect.any(Number),
+    //     distanceSinceStart: expect.any(Number),
+    //     fuelVolume: expect.any(Number),
+    //     antiLockBraking: expect.any(String),
+    //     engineCoolantTemperature: expect.any(Number),
+    //     engineTotalOperatingHours: expect.any(Number),
+    //     engineTotalFuelConsumption: expect.any(Number),
+    //     brakeFluidLevel: expect.any(String),
+    //     engineTorque: expect.any(Number),
+    //     engineLoad: expect.any(Number),
+    //     wheelBasedSpeed: expect.any(Number),
+    //     batteryLevel: expect.any(Number),
+    //     checkControlMessages: expect.objectContaining([
+    //       {
+    //         id: expect.any(Number),
+    //         remainingMinutes: expect.any(Number),
+    //         status: expect.any(String),
+    //         text: expect.any(String),
+    //       },
+    //     ]),
+    //     tirePressures: [
+    //       { location: 'front_left', pressure: expect.any(Number) },
+    //       { location: 'front_right', pressure: expect.any(Number) },
+    //       { location: 'rear_right', pressure: expect.any(Number) },
+    //       { location: 'rear_left', pressure: expect.any(Number) },
+    //     ],
+    //     tireTemperatures: [
+    //       { location: 'front_left', temperature: expect.any(Number) },
+    //       { location: 'front_right', temperature: expect.any(Number) },
+    //       { location: 'rear_right', temperature: expect.any(Number) },
+    //       { location: 'rear_left', temperature: expect.any(Number) },
+    //     ],
+    //     wheelRpms: [
+    //       { location: 'front_left', rpm: expect.any(Number) },
+    //       { location: 'front_right', rpm: expect.any(Number) },
+    //       { location: 'rear_right', rpm: expect.any(Number) },
+    //       { location: 'rear_left', rpm: expect.any(Number) },
+    //     ],
+    //     troubleCodes: expect.objectContaining([
+    //       {
+    //         occurences: expect.any(Number),
+    //         id: expect.any(String),
+    //         ecuId: expect.any(String),
+    //         status: expect.any(String),
+    //       },
+    //     ]),
+    //     mileageMeters: expect.any(Number),
+    //   })
+    // );
   });
 });
--- test/Responses/DiagnosticsResponse.spec.js
+++ test/Responses/DiagnosticsResponse.spec.js
@@ -6,55 +6,147 @@ describe(`DiagnosticsResponse`, () => {
   it(`should return DiagnosticsResponse`, () => {
     const response = new Response(
       hexToUint8Array(
-        '003301010006010003000bb802000501000200120300050100020000040005010002000005000b0100083fe999999999999a06000501000200c8090004010001000b0007010004414000000c0007010004000000000d000501000200000e000501000200000f00070100040000000010000401000100110005010002001712000701000441c00000130007010004441600001400040100010015000b0100083fc999999999999a16000b0100083fb999999999999a170005010002000018000b0100083fe999999999999a19000c010009000a000000000000001a000801000500401333331a000801000501401333331a000801000502401333331a000801000503401333331b000801000500422000001b000801000501422000001b000801000502422000001b000801000503422000001c00060100030000001c00060100030100001c00060100030200001c00060100030300001d0007010004000000001e000701000400000bb8a2000b01000800000168e701114b'
+        '00330101000f010003000bb802000601692482d62e02000e010002001202000601692482d62e03000e010002000002000601692482d62e04000e010002000002000601692482d62e0500140100083fe999999999999a02000601692482d62e06000e01000200c802000601692482d62e09000d0100010002000601692482d62e0b00100100044140000002000601692482d62e0c00100100040000000002000601692482d62e0d000e010002000002000601692482d62e0e000e010002000002000601692482d62e0f00100100040000000002000601692482d62e10000d0100010002000601692482d62e11000e010002001702000601692482d62e12001001000441c0000002000601692482d62e1300100100044416000002000601692482d62e14000d0100010002000601692482d62e1500140100083fc999999999999a02000601692482d62e1600140100083fb999999999999a02000601692482d62e17000e010002000002000601692482d62e1800140100083fe999999999999a02000601692482d62e190015010009000a0000000000000002000601692482d62e1a0011010005004013333302000601692482d62e1a0011010005014013333302000601692482d62e1a0011010005024013333302000601692482d62e1a0011010005034013333302000601692482d62e1b0011010005004220000002000601692482d62e1b0011010005014220000002000601692482d62e1b0011010005024220000002000601692482d62e1b0011010005034220000002000601692482d62e1c000f01000300000002000601692482d62e1c000f01000301000002000601692482d62e1c000f01000302000002000601692482d62e1c000f01000303000002000601692482d62e1d00100100040000000002000601692482d62e1e001001000400000bb802000601692482d62ea2001401000800000169248d4d270200060169248d4d27'
       )
     );
     expect(response.parse()).toBeInstanceOf(DiagnosticsResponse);
     expect(response.parse()).toEqual({
-      mileage: 3000,
-      engineOilTemperature: 18,
-      speed: 0,
-      engineRPM: 0,
-      fuelLevel: 0.8,
-      estimatedRange: 200,
-      washerFluidLevel: 'low',
-      batteryVoltage: 12,
-      adblueLevel: 0,
-      distanceSinceReset: 0,
-      distanceSinceStart: 0,
-      fuelVolume: 0,
-      antiLockBraking: 'inactive',
-      engineCoolantTemperature: 23,
-      engineTotalOperatingHours: 24,
-      engineTotalFuelConsumption: 600,
-      brakeFluidLevel: 'low',
-      engineTorque: 0.2,
-      engineLoad: 0.1,
-      wheelBasedSpeed: 0,
-      batteryLevel: 0.8,
+      mileage: { data: 3000, timestamp: new Date('2019-02-25T11:55:48.142Z') },
+      engineOilTemperature: {
+        data: 18,
+        timestamp: new Date('2019-02-25T11:55:48.142Z'),
+      },
+      speed: { data: 0, timestamp: new Date('2019-02-25T11:55:48.142Z') },
+      engineRPM: { data: 0, timestamp: new Date('2019-02-25T11:55:48.142Z') },
+      fuelLevel: { data: 0.8, timestamp: new Date('2019-02-25T11:55:48.142Z') },
+      estimatedRange: {
+        data: 200,
+        timestamp: new Date('2019-02-25T11:55:48.142Z'),
+      },
+      washerFluidLevel: {
+        data: 'low',
+        timestamp: new Date('2019-02-25T11:55:48.142Z'),
+      },
+      batteryVoltage: {
+        data: 12,
+        timestamp: new Date('2019-02-25T11:55:48.142Z'),
+      },
+      adblueLevel: { data: 0, timestamp: new Date('2019-02-25T11:55:48.142Z') },
+      distanceSinceReset: {
+        data: 0,
+        timestamp: new Date('2019-02-25T11:55:48.142Z'),
+      },
+      distanceSinceStart: {
+        data: 0,
+        timestamp: new Date('2019-02-25T11:55:48.142Z'),
+      },
+      fuelVolume: { data: 0, timestamp: new Date('2019-02-25T11:55:48.142Z') },
+      antiLockBraking: {
+        data: 'inactive',
+        timestamp: new Date('2019-02-25T11:55:48.142Z'),
+      },
+      engineCoolantTemperature: {
+        data: 23,
+        timestamp: new Date('2019-02-25T11:55:48.142Z'),
+      },
+      engineTotalOperatingHours: {
+        data: 24,
+        timestamp: new Date('2019-02-25T11:55:48.142Z'),
+      },
+      engineTotalFuelConsumption: {
+        data: 600,
+        timestamp: new Date('2019-02-25T11:55:48.142Z'),
+      },
+      brakeFluidLevel: {
+        data: 'low',
+        timestamp: new Date('2019-02-25T11:55:48.142Z'),
+      },
+      engineTorque: {
+        data: 0.2,
+        timestamp: new Date('2019-02-25T11:55:48.142Z'),
+      },
+      engineLoad: {
+        data: 0.1,
+        timestamp: new Date('2019-02-25T11:55:48.142Z'),
+      },
+      wheelBasedSpeed: {
+        data: 0,
+        timestamp: new Date('2019-02-25T11:55:48.142Z'),
+      },
+      batteryLevel: {
+        data: 0.8,
+        timestamp: new Date('2019-02-25T11:55:48.142Z'),
+      },
       checkControlMessages: [
-        { id: 10, remainingMinutes: 0, text: '', status: '' },
+        {
+          data: { id: 10, remainingMinutes: 0, text: '', status: '' },
+          timestamp: new Date('2019-02-25T11:55:48.142Z'),
+        },
       ],
       tirePressures: [
-        { location: 'front_left', pressure: 2.3 },
-        { location: 'front_right', pressure: 2.3 },
-        { location: 'rear_right', pressure: 2.3 },
-        { location: 'rear_left', pressure: 2.3 },
+        {
+          data: { location: 'front_left', pressure: 2.3 },
+          timestamp: new Date('2019-02-25T11:55:48.142Z'),
+        },
+        {
+          data: { location: 'front_right', pressure: 2.3 },
+          timestamp: new Date('2019-02-25T11:55:48.142Z'),
+        },
+        {
+          data: { location: 'rear_right', pressure: 2.3 },
+          timestamp: new Date('2019-02-25T11:55:48.142Z'),
+        },
+        {
+          data: { location: 'rear_left', pressure: 2.3 },
+          timestamp: new Date('2019-02-25T11:55:48.142Z'),
+        },
       ],
       tireTemperatures: [
-        { location: 'front_left', temperature: 40 },
-        { location: 'front_right', temperature: 40 },
-        { location: 'rear_right', temperature: 40 },
-        { location: 'rear_left', temperature: 40 },
+        {
+          data: { location: 'front_left', temperature: 40 },
+          timestamp: new Date('2019-02-25T11:55:48.142Z'),
+        },
+        {
+          data: { location: 'front_right', temperature: 40 },
+          timestamp: new Date('2019-02-25T11:55:48.142Z'),
+        },
+        {
+          data: { location: 'rear_right', temperature: 40 },
+          timestamp: new Date('2019-02-25T11:55:48.142Z'),
+        },
+        {
+          data: { location: 'rear_left', temperature: 40 },
+          timestamp: new Date('2019-02-25T11:55:48.142Z'),
+        },
       ],
       wheelRpms: [
-        { location: 'front_left', rpm: 0 },
-        { location: 'front_right', rpm: 0 },
-        { location: 'rear_right', rpm: 0 },
-        { location: 'rear_left', rpm: 0 },
+        {
+          data: { location: 'front_left', rpm: 0 },
+          timestamp: new Date('2019-02-25T11:55:48.142Z'),
+        },
+        {
+          data: { location: 'front_right', rpm: 0 },
+          timestamp: new Date('2019-02-25T11:55:48.142Z'),
+        },
+        {
+          data: { location: 'rear_right', rpm: 0 },
+          timestamp: new Date('2019-02-25T11:55:48.142Z'),
+        },
+        {
+          data: { location: 'rear_left', rpm: 0 },
+          timestamp: new Date('2019-02-25T11:55:48.142Z'),
+        },
       ],
-      troubleCodes: [{ occurences: 0, id: '', ecuId: '', status: '' }],
-      mileageMeters: 3000,
+      troubleCodes: [
+        {
+          data: { occurences: 0, id: '', ecuId: '', status: '' },
+          timestamp: new Date('2019-02-25T11:55:48.142Z'),
+        },
+      ],
+      mileageMeters: {
+        data: 3000,
+        timestamp: new Date('2019-02-25T11:55:48.142Z'),
+      },
     });
   });
 });
